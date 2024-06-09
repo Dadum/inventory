487ccbea492dceddcaeacad0b0f3a875eddcfd8b
dadum <sir.dadum@gmail.com>
2024-06-09 18:51:53 +0200

    Animate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:inventory/constants.dart';
@@ -217,7 +218,7 @@ class CharacterInventory extends ConsumerWidget {
                         dimension: 80,
                         child: ItemWidget(item: e),
                       ),
-                    ),
+                    ).animate().fade(),
                   )
                   .toList(),
             ),
